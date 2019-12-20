6c6213725e22f9a6b7173d113edcb24077af9e9b
lomtepradip <lomtepradip@gmail.com>
2019-12-20 19:46:22 +0530

    another commit

--- spring-boot-test/spring-boot-test/src/main/java/com/example/springboottest/HomeController.java
+++ spring-boot-test/spring-boot-test/src/main/java/com/example/springboottest/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
 	@GetMapping(path="/all")
-	public String home() {
+	public String home() {   
 		return "hello word from home controller";
 	}
 }
